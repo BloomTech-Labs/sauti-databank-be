d220068d490a84c514a8d3ee2dc1f00d620ecd65
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-07-16 14:31:15 -0700

    new changes for the cors

--- api/server.js
+++ api/server.js
@@ -4,10 +4,17 @@ const sessions = require("../routes/sessions-router")
 
 const server = express();
 
+server.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 server.use(helmet());
 server.use(express.json());
 server.use("/sessions", sessions);
+server.use(cors({origin: 'http://localhost:3000'}));
 
 server.get("/", (req, res) => {
   res.send("WE ARE UP!");
