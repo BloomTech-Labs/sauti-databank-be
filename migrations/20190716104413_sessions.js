7a0126a49150266a0cd82e42fd47e96b888c2f56
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-07-23 15:56:10 -0700

    Created migrations for first data model

--- migrations/20190716104413_sessions.js
+++ migrations/20190716104413_sessions.js
@@ -1,20 +1,72 @@
+// Building schema for data model 
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('sessions', sessions => {
-        sessions.increments();
 
-        sessions
-            .string("language", 100)
-            .notNullable()
-        sessions
-            .string("country", 100)
-            .notNullable()
-        sessions
-            .string("product", 100)
-            .notNullable()
+    // Users table
+    return knex.schema.createTable("users", users => {
+        
+        // Primary key 
+        users.increments();
+
+        // Gender
+        users
+            .string("gender", 10)
     })
-  
+
+    // Sessions table 
+    .createTable("sessions", sessions => {
+
+        sessions.increments(); 
+
+        // User id, foreign key 
+        sessions
+            .integer("user_id")
+            .references("id")
+            .inTable("users")
+            .onDelete("RESTRICT")
+            .onUpdate("CASCADE")
+
+        // Timestamp 
+        sessions.timestamp("date")
+    })
+
+    // Commodity table 
+    .createTable('commodity', commodity => {
+
+        commodity.increments(); 
+
+        // Name 
+        sessions
+            .string("name", 100)
+
+    })
+
+    // Commodity selection table 
+    .createTable('commodity_selection', commodity_selection => {
+
+        commodity_selection.increments(); 
+
+        // Commodity id, foreign key 
+        commodity_selection
+            .integer("commodity_id")
+            .references("id")
+            .inTable("commodity")
+            .onDelete("RESTRICT")
+            .onUpdate("CASCADE")
+        
+        // Session id, foreign key 
+        commodity_selection 
+            .integer("sessions_id")
+            .references("id")
+            .inTable("sessions")
+            .onDelete("RESTRICT")
+            .onUpdate("CASCADE")
+
+    })
 };
 
 exports.down = function(knex, Promise) {
+    knex.schema.dropTableIfExists('users');
     knex.schema.dropTableIfExists('sessions');
+    knex.schema.dropTableIfExists('commodity');
+    knex.schema.dropTableIfExists('commodity_selection');
 };
