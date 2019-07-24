2ead647f2568f52755791e2030f8cc1a68a6fc96
Vijay Das <vijaytdas@gmail.com>
2019-07-24 12:37:44 -0500

    add seeds for users, sessions, commodity, commodity_selection, able to confirm in MySQL db

---
+++ seeds/001_users_seed.js
@@ -0,0 +1,55 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex("users")
+    .del()
+    .then(function() {
+      // Inserts seed entries
+      return knex("users").insert([
+        {
+          gender: "female"
+        },
+        {
+          gender: "female"
+        },
+        {
+          gender: "female"
+        },
+        {
+          gender: "female"
+        },
+        {
+          gender: "female"
+        },
+        {
+          gender: "female"
+        },
+        {
+          gender: "female"
+        },
+        {
+          gender: "female"
+        },
+        {
+          gender: "female"
+        },
+        {
+          gender: "female"
+        },
+        {
+          gender: "male"
+        },
+        {
+          gender: "male"
+        },
+        {
+          gender: "male"
+        },
+        {
+          gender: "male"
+        },
+        {
+          gender: "male"
+        },
+      ]);
+    });
+};
