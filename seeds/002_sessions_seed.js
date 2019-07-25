e5ca83712772dc8fd2b2216e8a4176a57d2ab50f
cashcoder88 <cashglobe@gmail.com>
2019-07-25 16:20:20 -0600

    sessions seeds now with IDs and mapped randomly for percentages

--- seeds/002_sessions_seed.js
+++ seeds/002_sessions_seed.js
@@ -6,21 +6,21 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       // assume 1 session per user for dummy data but real world will have multiple sessions per user
       return knex("sessions").insert([
-        { user_id: 2, date: "2017-01-01 12:12:12" },
+        { user_id: 1, date: "2017-01-01 12:12:12" },
+        { user_id: 2, date: "2018-01-01 12:12:12" },
+        { user_id: 3, date: "2019-01-01 12:12:12" },
+        { user_id: 4, date: "2017-01-01 12:12:12" },
+        { user_id: 5, date: "2018-01-01 12:12:12" },
+        { user_id: 6, date: "2018-01-01 12:12:12" },
+        { user_id: 7, date: "2018-01-01 12:12:12" },
+        { user_id: 8, date: "2019-01-01 12:12:12" },
+        { user_id: 9, date: "2017-01-01 12:12:12" },
+        { user_id: 10, date: "2018-01-01 12:12:12" },
+        { user_id: 11, date: "2017-01-01 12:12:12" },
         { user_id: 12, date: "2018-01-01 12:12:12" },
-        { user_id: 22, date: "2019-01-01 12:12:12" },
-        { user_id: 32, date: "2017-01-01 12:12:12" },
-        { user_id: 42, date: "2018-01-01 12:12:12" },
-        { user_id: 52, date: "2017-01-01 12:12:12" },
-        { user_id: 62, date: "2018-01-01 12:12:12" },
-        { user_id: 72, date: "2019-01-01 12:12:12" },
-        { user_id: 82, date: "2017-01-01 12:12:12" },
-        { user_id: 92, date: "2018-01-01 12:12:12" },
-        { user_id: 102, date: "2017-01-01 12:12:12" },
-        { user_id: 112, date: "2018-01-01 12:12:12" },
-        { user_id: 122, date: "2019-01-01 12:12:12" },
-        { user_id: 132, date: "2017-01-01 12:12:12" },
-        { user_id: 142, date: "2018-01-01 12:12:12" }
+        { user_id: 13, date: "2019-01-01 12:12:12" },
+        { user_id: 14, date: "2017-01-01 12:12:12" },
+        { user_id: 15, date: "2017-01-01 12:12:12" }
       ]);
     });
 };
