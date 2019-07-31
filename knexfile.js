d1447283f03e2d798a54726d7655b4ac204a5c8c
Vijay Das <vijaytdas@gmail.com>
2019-07-31 11:35:22 -0500

    modified knexfile for Lance staging mysql, removed ENV references to cleardb

--- knexfile.js
+++ knexfile.js
@@ -5,46 +5,46 @@ module.exports = {
   development: {
     client: "mysql",
     connection: {
-      host: "localhost",
-      user: "root",
-      password: "password",
-      database: "sauti",
+      host: "160.153.141.46",
+      user: "lambda2019",
+      password: "JlgRzqmvh83Z",
+      database: "lambdaStaging",
       charset: "utf8"
     }
   },
 
-  staging: {
-    client: "mysql",
-    connection: process.env.CLEARDB_DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
+  // staging: {
+  //   client: "mysql",
+  //   connection: process.env.CLEARDB_DATABASE_URL,
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // },
 
-  production: {
-    client: "mysql",
-    connection: process.env.CLEARDB_DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-  testing: {
-    client: "mysql",
-    connection: {
-      host: "localhost",
-      user: "root",
-      password: "password",
-      database: "sauti-test",
-      charset: "utf8"
-    },
-    useNullAsDefault: true,
-  }
+  // production: {
+  //   client: "mysql",
+  //   connection: process.env.CLEARDB_DATABASE_URL,
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations"
+  //   }
+  // },
+  // testing: {
+  //   client: "mysql",
+  //   connection: {
+  //     host: "localhost",
+  //     user: "root",
+  //     password: "password",
+  //     database: "sauti-test",
+  //     charset: "utf8"
+  //   },
+  //   useNullAsDefault: true,
+  // }
 };
