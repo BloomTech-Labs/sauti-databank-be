c5edf2a29ee035ed68eff8e83d85a0ad21e59ebd
joerlop <jj.erlich155@gmail.com>
2019-08-07 17:14:19 -0500

    Users data endpoint developed

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const sessions = require("../routes/sessions-router")
+const users = require("../routes/users-router")
 const cors = require("cors");
 
 const server = express();
@@ -17,6 +18,7 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/sessions", sessions);
+server.use("/users", users);
 
 server.get("/", (req, res) => {
   res.status(200).send("WE ARE UP!");
