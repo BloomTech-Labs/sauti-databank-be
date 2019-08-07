a5a873c3cbbe9c0b559a4bc02d48853acc2cad77
Vijay Das <vijaytdas@gmail.com>
2019-08-07 18:29:29 -0500

    model and endpoints for educations

--- routes/users-router.js
+++ routes/users-router.js
@@ -13,4 +13,66 @@ router.get("/all", (req, res) => {
     });
 });
 
+// education routes
+// select * from users where users.education 
+
+router.get("/all/education/all", (req, res) => {
+
+  Users.getEducation()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+router.get("/all/education/all/primary", (req, res) => {
+
+  Users.getEducationPrimary()
+    .then(users => {
+      res.status(200).json(users.length);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+
+router.get("/all/education/all/secondary", (req, res) => {
+
+  Users.getEducationSecondary()
+    .then(users => {
+      res.status(200).json(users.length);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+
+router.get("/all/education/all/uni", (req, res) => {
+
+  Users.getEducationUni()
+    .then(users => {
+      res.status(200).json(users.length);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+
+router.get("/all/education/all/none", (req, res) => {
+
+  Users.getEducationNone()
+    .then(users => {
+      res.status(200).json(users.length);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+
 module.exports = router;
