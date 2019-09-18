9e220f1e613b70d9552a07cd1a74c8b3449e1236
azrap <azraap@gmail.com>
2019-09-18 09:55:25 -0700

    added more comments

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,11 @@
 // Update with your config settings.
 require("dotenv").config();
+
 console.log(process.env.host);
 
 module.exports = {
   development: {
     client: "mysql",
-    // connection: {
-    //   host: "160.153.141.46",
-    //   user: "lambda2019",
-    //   password: "JlgRzqmvh83Z",
-    //   database: "lambdaStaging",
-    //   charset: "utf8"
-    // },
     connection: {
       host: process.env.host,
       user: process.env.user,
