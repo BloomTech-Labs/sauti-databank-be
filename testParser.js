8951edbcdeee6efa676b73313c5c762e5681837d
azrap <azraap@gmail.com>
2019-09-18 09:53:18 -0700

    added comments

--- testParser.js
+++ testParser.js
@@ -3,20 +3,27 @@ const Sessions = require("./routes/sessions-model");
 let unserializer = require("php-unserialize");
 const bcrypt = require("bcryptjs");
 
-try {
-  Sessions.findLanceData().then(sessions => {
-    console.log(sessions.length);
-    var array = sessions.filter(element => {
-      return element.data.length > 0;
-    });
+// make sure you have php-unserialize in your package.json. If not, yarn add php-unserialize
 
-    var newArr = array.slice(0, 10);
-    newArr.map((element, index) => {
-      console.log(`${index}`);
-      console.log(element.data);
-      console.log(unserializer.unserialize(element.data));
-    });
-  });
+try {
+  //fetch all rows from the platform_sessions data
+  Sessions.findLanceData().then(
+    //taking sessions data
+    sessions => {
+      console.log(sessions.length);
+      var array = sessions.filter(element => {
+        return element.data.length > 0;
+      });
+
+      var newArr = array.slice(0, 10);
+      newArr.map((element, index) => {
+        console.log(`${index}`);
+        console.log(element.data);
+        console.log(unserializer.unserialize(element.data));
+      });
+    }
+  );
 } catch ({ message }) {
   console.log(message);
 }
+//to run this script on the command line, type:  node testParser.js
