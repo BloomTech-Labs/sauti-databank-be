bbb96a74be8c6a00bcb2dba49032c10b0c81be21
Greg Jordan <gregljordan@gmail.com>
2019-10-01 23:42:42 -0400

    Completed if/else statements and produced data

--- infoDemandParser.js
+++ infoDemandParser.js
@@ -9,7 +9,7 @@ try {
   Sessions.findLanceData().then(
     //sessions is the entire platform sessions table(array)
     sessions => {
-      console.log(sessions.length);
+      console.log('sessions size', sessions.length);
 
       //sessions filtering through each row and grabbing/getting each row that has non-zero data field
       //we don't need to unserialize if it doesn't have data
@@ -17,9 +17,9 @@ try {
         return row.data.length > 0;
       });
 
-      console.log(array.length);
+      console.log('Array size', array.length);
       //slicing the array to condense the amount of data retrieved/taking a subset of 2 sessions:
-      let newArr = array.slice(0, 2);
+      let newArr = array.slice(0, 4);
       // making a new array
       let infoArr = [];
       //looping through the new array
@@ -27,10 +27,11 @@ try {
         // we created a variable and now we are unserializing the data that we looped through
         const data = unserializer.unserialize(serializedRow.data);
 
-        console.log(serializedRow); //serialized row of data
-        console.log(data); //unserialized data
+        console.log('Row', serializedRow); //serialized row of data
+        console.log('data object', data); //unserialized data
         //Object.keys returning the enumerable keys from the data, looping through each key and pushing this info into the empty infoArr
         Object.keys(data).forEach((keyEle, index) => {
+          console.log('Key', keyEle)
           if (keyEle === 'procedurecommodity') {
             infoArr.push({
               id: infoArr.length, //incrementing the id by the length of the array
@@ -121,12 +122,13 @@ try {
             });
           }
         });
-        console.log(infoArr); //Seeing if the array now has access to the key value pairs(data)
+        console.log('infoArr', infoArr); //Seeing if the array now has access to the key value pairs(data)
       });
     }
+
   );
 } catch ({ message }) {
-  console.log(message);
+  console.log('message', message);
 } // if we don't successfully retrieve the data we should see an error message
 //to run this script on the command line, type:  node testParser.js
 
