7e9371f8000b64d1bf6f05ca9c7bd356b746d817
Melanie Harris <mell.j.harris@gmail.com>
2019-10-02 17:03:45 -0700

    populated infoDemand table

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
-const sessions = require("../routes/sessions-router")
-const users = require("../routes/users-router")
+const sessions = require("../routes/sessions-router");
+const users = require("../routes/users-router");
 const cors = require("cors");
+const infoDemand = require('../routes/infodemand-router');
 
 const server = express();
 
@@ -12,6 +13,7 @@ server.use(cors());
 
 server.use("/sessions", sessions);
 server.use("/users", users);
+server.use("/info", infoDemand);
 
 server.get("/", (req, res) => {
   res.status(200).send("WE ARE UP!");
--- routes/infodemand-model.js
+++ routes/infodemand-model.js
@@ -1,8 +1,18 @@
 const db = require('../data/dbConfig')
+const DRYParser = require('../DRYParser')
 
+// const add = (info_row) => db('information_demand').insert(info_row)
 
-const add = (info_row) => db('information_demand').insert(info_row)
+const add = async function(info_row){
+    // console.log(x, "row")
+    x = await db('information_demand').insert(info_row)
+    console.log(x,'id?')
+    return x
+}
+
+const get = () => db('information_demand')
 
 module.exports = {
     add,
+    get,
 }
