d49e04d57e48fe6e2aa8886cdd9863f9f3f6214b
Greg Jordan <gregljordan@gmail.com>
2019-10-07 16:46:39 -0400

    Added if/else statements to infoPro-router to output string values

--- tab2-routes/infoPro-model.js
+++ tab2-routes/infoPro-model.js
@@ -5,7 +5,11 @@ module.exports = {
 } // export all the helper functions you create 
 
 // create a function to locate the document information for procedures get
+
+
 function getInfoPro(){
     return db('information_demand').where({request_type_id: '4'})
-}// reach a specific portion of the db, specifically locate the table you want to extract these data points from 
+}
+
+// reach a specific portion of the db, specifically locate the table you want to extract these data points from 
 //.where is specifically pointing to the portion of the table that you want to access 
