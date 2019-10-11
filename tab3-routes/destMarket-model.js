5fbdc16106872f9c6588316b75aa7586a23684f2
Greg Jordan <gregljordan@gmail.com>
2019-10-11 16:27:21 -0400

    Added component labels

--- tab3-routes/destMarket-model.js
+++ tab3-routes/destMarket-model.js
@@ -4,6 +4,8 @@ module.exports = {
     getDestMarket
 };
 
+// Final Destination Market
+
 function getDestMarket() {
     return db.raw('select * from information_demand where request_type_id = 8')
 };
--- tab3-routes/destMarket-router.js
+++ tab3-routes/destMarket-router.js
@@ -3,15 +3,16 @@ const router = express.Router();
 
 const DestMarket = require('./destMarket-model');
 
+// Final Destination Market
+
 router.get('/', (req, res) => {
-    DestMarket
-        .getDestMarket()
-        .then(market => {
-            res.status(200).json(market);
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
+	DestMarket.getDestMarket()
+		.then(market => {
+			res.status(200).json(market);
+		})
+		.catch(err => {
+			res.status(500).json(err);
+		});
 });
 
 module.exports = router;
