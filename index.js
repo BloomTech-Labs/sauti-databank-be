9c96ab4e36a40643e032a24a08515327fa4e7155
Robert <rc3331@gmail.com>
2019-12-16 11:00:59 -0600

    initial setup of graphql server files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const server = require("./api/server");
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`\n** Running on port ${port} **\n`);
 });
