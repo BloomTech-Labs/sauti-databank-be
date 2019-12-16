140445885936fa4ff06de317cecdb84b2931846b
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-12-16 13:16:33 -0800

    added commodities to schema, will add more?

--- graphQL/schema.js
+++ graphQL/schema.js
@@ -2,7 +2,8 @@ const { buildSchema } = require('graphql');
 const schema = buildSchema(`
     type Query {
         tradersData(request_value: String): [SautiTrader]
-        tradersUsers(limit: Int, gender: String, age: String, education: String): [User]
+        tradersUsers(limit: Int, age: Int): [User]
+        tradersCommodities(produce: String): [Commodity]
     }
 
     type SautiTrader {
@@ -23,8 +24,23 @@ const schema = buildSchema(`
         education: String
         crossing_freq: String
         produce: String
+        primary)income: String
         language: String
         country_of_residence: String
+    }
+
+    type Commodity {
+        id: Int
+        source: String
+        market: String
+        country: String
+        product_cat: String
+        product_agg: String
+        product: String
+        retail: Int
+        wholesale: Int
+        currency: String
+        unit: Int
     }
 `);
 
