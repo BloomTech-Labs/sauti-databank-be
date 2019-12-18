3b60572c91381e552e7a692e494b3b81622fa134
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-12-17 16:37:19 -0800

    renaming remodels removed

--- models/model.js
+++ models/model.js
@@ -9,18 +9,18 @@ const getSessions = () => {
       .join('information_demand as id', 'id.cell_num', 't.cell_num')
       .join('request_type as rt', 'id.request_type_id', 'rt.id')
       .select(
-         't.id as ID',
-         't.cell_num as Phone',
-         't.gender as Gender',
-         't.age as Age',
-         't.education as Education',
-         't.crossing_freq as Crossing Frequency',
-         't.produce as Produce',
-         't.primary_income as Primary Income',
-         't.language as Language',
-         't.country_of_residence as Country of Residence',
-         'rt.request_type as Request Type',
-         'id.request_value as Request Value'
+         't.id',
+         't.cell_num',
+         't.gender',
+         't.age',
+         't.education',
+         't.crossing_freq',
+         't.produce',
+         't.primary_income',
+         't.language',
+         't.country_of_residence',
+         'rt.request_type',
+         'id.request_value'
       )
 }
 
