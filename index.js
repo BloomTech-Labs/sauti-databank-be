a6e7591cd81348d9f1cd57bd0760eefbfe12ba6f
Dylan Mestyanek <mestyanekdylan@gmail.com>
2020-01-23 09:59:08 -0600

    Removed remnants of cron testing files.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const server = require("./server");
 
 const port = process.env.PORT || 4000;
+
 server.listen(port, () => {
   console.log(`\n** Running on port ${port} **\n`);
 });
--- models/cronTest.js
+++
@@ -1,7 +0,0 @@
-let testNumber = 0
-
-const addOne = () => {
-    testNumber += 1
-    console.log('This is how many times the cron job ran', testNumber)
-    return testNumber
-}
