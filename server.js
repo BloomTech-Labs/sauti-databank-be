cdc40813d935cd5b7098d54354bee8d5900516fc
lukasjaronis <jaronisluke13@gmail.com>
2020-02-19 14:34:51 -0500

    Updated server.js | Added cors

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 const helmet = require("helmet");
 const { ApolloServer } = require("apollo-server")
+const cors = require("cors");
 const typeDefs = require("./graphQL/schema");
 const resolvers = require("./graphQL/resolvers");
 const port = process.env.PORT || 2500;
 const model = require("./models/model")
 
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "OPTIONS"],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Content-Length', 'X-Requested-With', 'Accept'],
+};
+
 const server = new ApolloServer({
-  cors: true,
+  corsOptions,
   helmet,
   typeDefs,
   resolvers,
