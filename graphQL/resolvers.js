8c6e36c1f37dd6f71352e03291b80f0203f2293d
Moses Samuel <mosamuel98@gmail.com>
2020-02-20 10:15:40 -0800

    fix bug that was causing app to crash because of undefined resolvers
    Co-authored-by: Lukas <jaronisluke13@gmail.com>
    Co-authored-by: Joshua Hill <Josh_Hill15@me.com>
    Co-authored-by: David Tauraso <david318@me.com>

--- graphQL/resolvers.js
+++ graphQL/resolvers.js
@@ -1,33 +1,33 @@
-const data = require("../models/model");
-
-// Used to get data from "traders" table only
-const getTraders = async args => {
-    const traderUsers = await data.getTraders();
-
-    let filtered = traderUsers;
-
-    for (let arg in args) {
-        filtered = filtered.filter(trader => trader[arg] === args[arg] )
-    }
-
-    return filtered;
-}
-
-// Used to get data from "traders" AND "parsed_data" table joined together
-const getDataSessions = async args => {
-    const traderUsers = await data.getDataSessions();
-
-    let filtered = traderUsers;
-
-    for (let arg in args) {
-        filtered = filtered.filter(trader => trader[arg] === args[arg] )
-    }
-
-    return filtered;
-}
-
-
 module.exports = {
-    getTraders,
-    getDataSessions
-}
+  Query: {
+    // Used to get data from "traders" table only
+    async tradersUsers(_, args, ctx) {
+      const keys = Object.keys(args);
+
+      if (!keys.length) {
+        return ctx.getTraders();
+      }
+      let dataFromDataBase;
+      for (let i = 0; i < keys.length; i++) {
+        if (i === 0) dataFromDataBase = await ctx.getTraders();
+        dataFromDataBase = dataFromDataBase.filter(
+          filterBy => filterBy[keys[i]] === args[keys[i]]
+        );
+      }
+      return dataFromDataBase;
+    },
+    // Used to get data from "traders" AND "parsed_data" table joined together
+    async sessionsData(_, args, ctx) {
+      const keys = Object.keys(args);
+      if (!keys.length) return ctx.getDataSessions();
+      let dataFromDataBase;
+      for (let i = 0; i < keys.length; i++) {
+        if (i === 0) dataFromDataBase = await ctx.getDataSessions();
+        dataFromDataBase = dataFromDataBase.filter(
+          filterBy => filterBy[keys[i]] === args[keys[i]]
+        );
+      }
+      return dataFromDataBase;
+    }
+  }
+};
