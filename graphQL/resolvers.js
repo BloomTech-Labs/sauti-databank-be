3b42f3da0f417afed4ff3f9b869846977622df21
Josh Hill <josh_hill15@me.com>
2020-02-24 11:40:03 -0500

    organize the backend

--- graphQL/resolvers.js
+++ graphQL/resolvers.js
@@ -5,51 +5,48 @@ const { JWT_SECRET: secret } = require("../config/secrets");
 module.exports = {
   Query: {
     // Used to get data from "traders" table only
-    async tradersUsers(_, args, ctx) {
-      const keys = Object.keys(args);
-
-      if (!keys.length) return ctx.Traders.getTraders();
-
+    async tradersUsers(_, { input }, ctx) {
+      if (!input) return ctx.Traders.getTraders()
+      const keys = Object.keys(input);
       let dataFromDataBase;
       for (let i = 0; i < keys.length; i++) {
         if (i === 0) dataFromDataBase = await ctx.Traders.getTraders();
         dataFromDataBase = dataFromDataBase.filter(
-          filterBy => filterBy[keys[i]] === args[keys[i]]
+          filterBy => filterBy[keys[i]] === input[keys[i]]
         );
       }
       return dataFromDataBase;
     },
-    // Used to get data from "traders" AND "parsed_data" table joined together
-    async sessionsData(_, args, ctx) {
-      const keys = Object.keys(args);
-      if (!keys.length) return ctx.Traders.getDataSessions();
+    // Used to get data from "parsed_data" and "traders" table joined
+    async sessionsData(_, { input }, ctx) {
+      if (!input) return ctx.Traders.getDataSessions();
+      const keys = Object.keys(input);
       let dataFromDataBase;
       for (let i = 0; i < keys.length; i++) {
         if (i === 0) dataFromDataBase = await ctx.Traders.getDataSessions();
         dataFromDataBase = dataFromDataBase.filter(
-          filterBy => filterBy[keys[i]] === args[keys[i]]
+          filterBy => filterBy[keys[i]] === input[keys[i]]
         );
       }
       return dataFromDataBase;
     },
-
     databankUser(_, args, ctx) {
       return ctx.Users.findAll();
     }
   },
   Mutation: {
-    async register(_, args, ctx) {
-      const hashedPassword = bcrypt.hashSync(args.password, 8);
+    async register(_, { input }, ctx) {
+      const hashedPassword = bcrypt.hashSync(input.password, 8);
       const [newlyCreatedUser] = await ctx.Users.create({
-        ...args,
+        ...input,
         password: hashedPassword
       });
       // leave out the stored password when returning the user object.
       const { password, ...newlyCreatedUserWithoutPassword } = newlyCreatedUser;
       return newlyCreatedUserWithoutPassword;
     },
-    async login(_, args, ctx) {
-      let user = args;
+    async login(_, { input }, ctx) {
+      let user = input;
       // if their login is valid
       if (await validPassword(user, ctx)) {
         const token = generateToken(user);
--- graphQL/schema.js
+++ graphQL/schema.js
@@ -49,82 +49,78 @@ const typeDefs = gql`
     job_position: String
     country: String
     token: String
+    organization_type: String
+  }
+
+  input newTraderInput {
+    id: Int
+    gender: String
+    age: String
+    education: String
+    crossing_freq: String
+    produce: String
+    primary_income: String
+    language: String
+    country_of_residence: String
+  }
+
+  input newTraderSessionInput {
+    id: Int
+    gender: String
+    age: String
+    education: String
+    crossing_freq: String
+    produce: String
+    primary_income: String
+    language: String
+    country_of_residence: String
+    procedurecommodity: String
+    procedurecommoditycat: String
+    proceduredest: String
+    procedurerequireddocument: String
+    procedurerelevantagency: String
+    procedureorigin: String
+    commoditycountry: String
+    commoditymarket: String
+    commoditycat: String
+    commodityproduct: String
+    exchangedirection: String
+    created_date: Date
+  }
+
+  input newRegisterInput {
+    id: Int
+    email: String!
+    password: String!
+    tier: String!
+    interest: String
+    organization: String
+    job_position: String
+    country: String
+    organization_type: String
+  }
+
+  input newLoginInput {
+    id: Int
+    email: String!
+    password: String!
+    tier: String
+    interest: String
+    organization: String
+    job_position: String
+    country: String
+    organization_type: String
   }
 
   type Query {
-    tradersUsers(
-      id: Int
-      gender: String
-      age: String
-      education: String
-      crossing_freq: String
-      produce: String
-      primary_income: String
-      language: String
-      country_of_residence: String
-      procedurecommodity: String
-      procedurecommoditycat: String
-      proceduredest: String
-      procedurerequireddocument: String
-      procedurerelevantagency: String
-      procedureorigin: String
-      commoditycountry: String
-      commoditymarket: String
-      commoditycat: String
-      commodityproduct: String
-      exchangedirection: String
-      created_date: Date
-    ): [TraderUser]
-
-    sessionsData(
-      id: Int
-      gender: String
-      age: String
-      education: String
-      crossing_freq: String
-      produce: String
-      primary_income: String
-      language: String
-      country_of_residence: String
-      procedurecommodity: String
-      procedurecommoditycat: String
-      proceduredest: String
-      procedurerequireddocument: String
-      procedurerelevantagency: String
-      procedureorigin: String
-      commoditycountry: String
-      commoditymarket: String
-      commoditycat: String
-      commodityproduct: String
-      exchangedirection: String
-      created_date: Date
-    ): [TraderSession]
-
-    databankUser: [databankUser]
+    tradersUsers(input: newTraderInput): [TraderUser]!
+    sessionsData(input: newTraderSessionInput): [TraderSession]!
+    databankUser: [databankUser]!
   }
 
   type Mutation {
-    register(
-      id: Int
-      email: String!
-      password: String!
-      tier: String!
-      interest: String
-      organization: String
-      job_position: String
-      country: String
-    ): databankUser!
-
-    login(
-      id: Int
-      email: String!
-      password: String!
-      tier: String
-      interest: String
-      organization: String
-      job_position: String
-      country: String
-    ): databankUser!
+    register(input: newRegisterInput!): databankUser!
+    login(input: newLoginInput!): databankUser!
   }
 `;
 
