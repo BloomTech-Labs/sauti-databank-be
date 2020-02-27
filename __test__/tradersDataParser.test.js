bcff9c72582cb748aa4c5a7ebf2f5056c57c2513
Moses Samuel <mosamuel98@gmail.com>
2020-02-27 14:54:45 -0800

    simplify the getTraders test

--- __test__/tradersDataParser.test.js
+++ __test__/tradersDataParser.test.js
@@ -2,12 +2,10 @@ test("getTraders() returns an array of traders' data", () => {
   // import dependencies
   const storedTradersData = require("../tmp/traders.json");
   const Traders = require("../models/model");
-
-  const traders = Traders.getTraders().then(traders => {
-    // map over each entry to remove the "Row Data Packet" prefix that was coming from the database.
-    return traders.map(trader => ({ ...trader }));
-  });
-  expect(traders).resolves.toEqual(storedTradersData);
+  // arrange
+  const allTraders = Traders.getTraders();
+  // assert
+  expect(allTraders).resolves.toEqual(storedTradersData);
 });
 
 describe("helpers", () => {
