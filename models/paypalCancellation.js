e43065d7fe637a51ec9e3f7b0722c697b91b99a7
Josh Hill <josh_hill15@me.com>
2020-03-25 18:11:16 -0400

    refactor cron job

--- models/paypalCancellation.js
+++ models/paypalCancellation.js
@@ -7,27 +7,35 @@ const qs = require("qs");
 // If thier subscription period is equal to the current date, this cron job will revert their account back to free.
 const job = new CronJob(
   // every 24 hours
-  "0 */24 * * *",
-  async function() {
+  // "0 */24 * * *",
+  "0 * * * *",
+  async function () {
     console.log("This cron job will run every 24 hours.");
+    // When the user cancels their subscription through our app, we set thte p_next_billing_time field
+    // to their next billing date. If this field is null, it means that the user hasn't cancelled their subsription.
     const cancelledSubs = (await DatabankUsers.findAll())
-      // When the user cancels their subscription through our app, we set thte p_next_billing_time field
-      // to their next billing date. If this field is null, it means that the user hasn't cancelled their subsription.
       .filter(user => user.p_next_billing_time !== null)
-      .map(user => {
-        return {
-          ...user,
-          p_next_billing_time: formatDate(user.p_next_billing_time)
-        };
-      });
-
+    // .map(user => {
+    //   return {
+    //     ...user,
+    //     p_next_billing_time: formatDate(user.p_next_billing_time)
+    //   };
+    // });
+    console.log("cancelled", cancelledSubs.length)
     let todaysDateFormatted = formatDate(new Date());
 
     cancelledSubs.forEach(async function changeUsersTiersToFree(subscriber) {
+      try {
+        var { data: { access_token }, config: authCreds } = await getAccessTokenAndAuthCreds("https://api.sandbox.paypal.com/v1/oauth2/token");
+        var { data: { billing_info: { next_billing_time } } } = await axios.get(`https://api.sandbox.paypal.com/v1/billing/subscriptions/${subscriber.subscription_id}`)
+      } catch (err) {
+        console.log("err", err)
+      }
+      console.log("next", access_token)
+      console.log("next_billing_time", formatDate(next_billing_time))
       if (subscriber.p_next_billing_time === todaysDateFormatted) {
-        // Cancel the user's subscription
+        // Cancel the users' subscriptions
         try {
-          const authCreds = await getAuthCreds();
           await axios.post(
             `https://api.sandbox.paypal.com/v1/billing/subscriptions/${subscriber.subscription_id}/cancel`,
             authCreds
@@ -58,8 +66,7 @@ function formatDate(date) {
   return new Date(date).toDateString();
 }
 
-async function getAuthCreds() {
-  const url = "https://api.sandbox.paypal.com/v1/oauth2/token";
+async function getAccessTokenAndAuthCreds(url) {
   const oldData = {
     grant_type: "client_credentials"
   };
@@ -87,5 +94,5 @@ async function getAuthCreds() {
     headers: { Authorization: `Bearer ${access_token}` }
   };
 
-  return config;
+  return { config, data };
 }
