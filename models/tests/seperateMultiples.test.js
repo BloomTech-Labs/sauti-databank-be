2495f4f8232d22751fd610734c1c3cbdf6ffba74
Alex Miller <alexmillerprojects@gmail.com>
2020-05-21 11:14:44 -0700

    added more tests for duplicate objects for separateMultiples parser

--- models/tests/seperateMultiples.test.js
+++ models/tests/seperateMultiples.test.js
@@ -1,14 +1,210 @@
-const seperateMultiples = require("../seperateMultiples");
-const mockData = require("./testDummyData");
+describe("creates arrays with new objects that are the correct size and shape for each search in a session", () => {
+  const seperateMultiples = require("../seperateMultiples");
+  beforeEach(() => {
+    jest.resetModules();
+  });
+  afterEach(() => {
+    jest.resetModules();
+  });
+  test(`returns an array`, () => {
+    const mockData = require("./testDummyData");
+    expect(Array.isArray(seperateMultiples(mockData))).toBe(true);
+  });
+  test(`array is full of objects`, () => {
+    const mockData = require("./testDummyData");
+    for (var i = 0; i < mockData.length; i++) {
+      expect(typeof seperateMultiples(mockData)[i]).toEqual("object");
+    }
+  });
+  test(`objects are the correct length`, () => {
+    const mockData = require("./testDummyData");
+    for (var i = 0; i < mockData.length; i++) {
+      expect(Object.keys(seperateMultiples(mockData)[i]).length).toEqual(14);
+    }
+  });
+  test(`creates new objects for each search in a session`, () => {
+    const mockData = require("./testDummyData");
+    let count = 0;
+    for (var i = 0; i < mockData.length; i++) {
+      Object.entries(mockData[i]).forEach(entry => {
+        if (typeof entry[1] === "string" && entry[1].includes(",")) {
+          ++count;
+        }
+      });
+    }
+    expect(mockData.length + count).toEqual(seperateMultiples(mockData).length);
+  });
+});
+describe("all new objects have undefined for thier properties excluding the new entry from the additional search", () => {
+  const seperateMultiples = require("../seperateMultiples");
+  beforeEach(() => {
+    jest.resetModules();
+  });
+  afterEach(() => {
+    jest.resetModules();
+  });
+  test(`new object session id is a number`, () => {
+    const mockData = require("./testDummyData");
+    let count = 0;
+    for (var i = 0; i < mockData.length; i++) {
+      Object.entries(mockData[i]).forEach(entry => {
+        if (typeof entry[1] === "string" && entry[1].includes(",")) {
+          ++count;
+        }
+      });
+    }
+    let newObjNum = seperateMultiples(mockData).length - count;
+    for (var j = newObjNum; j < seperateMultiples(mockData).length; j++) {
+      Object.entries(seperateMultiples(mockData)[j]).forEach(entry => {
+        if (entry[0] === "platform_sessions_id") {
+          expect(typeof entry[1]).toEqual("number");
+        }
+      });
+    }
+  });
+  test(`new object cell number is a string`, () => {
+    const mockData = require("./testDummyData");
+    let count = 0;
+    for (var i = 0; i < mockData.length; i++) {
+      Object.entries(mockData[i]).forEach(entry => {
+        if (typeof entry[1] === "string" && entry[1].includes(",")) {
+          ++count;
+        }
+      });
+    }
+    let newObjNum = seperateMultiples(mockData).length - count;
+    for (var j = newObjNum; j < seperateMultiples(mockData).length; j++) {
+      Object.entries(seperateMultiples(mockData)[j]).forEach(entry => {
+        if (entry[0] === "cell_num") {
+          expect(typeof entry[1]).toEqual("string");
+        }
+      });
+    }
+  });
+  test(`new object created date is a string`, () => {
+    const mockData = require("./testDummyData");
+    let count = 0;
+    for (var i = 0; i < mockData.length; i++) {
+      Object.entries(mockData[i]).forEach(entry => {
+        if (typeof entry[1] === "string" && entry[1].includes(",")) {
+          ++count;
+        }
+      });
+    }
+    let newObjNum = seperateMultiples(mockData).length - count;
+    for (var j = newObjNum; j < seperateMultiples(mockData).length; j++) {
+      Object.entries(seperateMultiples(mockData)[j]).forEach(entry => {
+        if (entry[0] === "created_date") {
+          expect(typeof entry[1]).toEqual("string");
+        }
+      });
+    }
+  });
+  test(`new object contains a number and three strings with all other fields undefined`, () => {
+    const mockData = require("./testDummyData");
+    let count = 0;
+    for (var i = 0; i < mockData.length; i++) {
+      Object.entries(mockData[i]).forEach(entry => {
+        if (typeof entry[1] === "string" && entry[1].includes(",")) {
+          ++count;
+        }
+      });
+    }
+    let newObjNum = seperateMultiples(mockData).length - count;
+    for (var j = newObjNum; j < seperateMultiples(mockData).length; j++) {
+      let countOfNumbers = 0;
+      let countOfStrings = 0;
+      let countUndefined = 0;
+      Object.entries(seperateMultiples(mockData)[j]).forEach(entry => {
+        if (typeof entry[1] === "number") {
+          ++countOfNumbers;
+        }
+        if (typeof entry[1] === "string") {
+          ++countOfStrings;
+        }
+        if (typeof entry[1] === "undefined") {
+          ++countUndefined;
+        }
+      });
+      expect(countOfStrings).toEqual(3);
+      expect(countUndefined).toEqual(10);
+      expect(countOfNumbers).toEqual(1);
+    }
+  });
+});
 
-test(`creates new objects`, () => {
-  let count = 0;
-  for (var i = 0; i < mockData.length; i++) {
-    Object.entries(mockData[i]).forEach(entry => {
-      if (typeof entry[1] === "string" && entry[1].includes(",")) {
-        ++count;
+describe("duplicate tests", () => {
+  const seperateMultiples = require("../seperateMultiples");
+  //USE THIS IF YOU ARE GOING TO USE "require('./testDummyData')" more than once
+  // beforeEach(() => {
+  //   jest.resetModules();
+  // });
+  test("After running the function with and array that has 2 extra object values, it removes those dupplicates(-2) ", async () => {
+    const testData1 = require("./testDummyData");
+    /*
+        The function will run, takes in "testData1" array as an arguement, 
+        and returns the new array without multiple values per property.
+        This test will be validatng this by checking the lengths 
+        of data before and after(theReturned) .
+        */
+    //this is the returned values length
+    const before = testData1;
+    console.log("length of array before function", testData1.length); //8
+    const theReturnedValue = seperateMultiples(new Array(testData1)[0]);
+    console.log("length of array after function", theReturnedValue.length); //6
+    // this is the length of the array before the function is used
+    // expect(theReturnedValue.length).toBeLessThan(8);
+    // expect(theReturnedValue.length).toBeLessThan(new Array(testData1)[0].length)
+  });
+  test("function does nothing when array has no duplicated data is sent to it", async () => {
+    /*
+        The function will run, takes in "noDupes" array as an arguement, 
+        and returns a new array with the same values per property the 
+        function does not run through the loops to remove dupplicates
+         because they're are none.
+        */
+    const noDupes = [
+      {
+        platform_sessions_id: 45773,
+        cell_num: "250000045773",
+        procedurecommodity: undefined,
+        procedurecommoditycat: undefined,
+        proceduredest: undefined,
+        procedurerequireddocument: undefined,
+        procedurerelevantagency: undefined,
+        procedureorigin: undefined,
+        commoditycountry: "RWA",
+        commoditymarket: undefined,
+        commoditycat: undefined,
+        commodityproduct: undefined,
+        exchangedirection: undefined,
+        created_date: "2020-04-24T17:31:40.000Z"
       }
-    });
-  }
-  expect(mockData.length + count).toEqual(seperateMultiples(mockData).length);
+    ];
+    //they are the same
+    const noDupesStill = [
+      {
+        platform_sessions_id: 45773,
+        cell_num: "250000045773",
+        procedurecommodity: undefined,
+        procedurecommoditycat: undefined,
+        proceduredest: undefined,
+        procedurerequireddocument: undefined,
+        procedurerelevantagency: undefined,
+        procedureorigin: undefined,
+        commoditycountry: "RWA",
+        commoditymarket: undefined,
+        commoditycat: undefined,
+        commodityproduct: undefined,
+        exchangedirection: undefined,
+        created_date: "2020-04-24T17:31:40.000Z"
+      }
+    ];
+    /*Here I'll save it
+        in a variable as a string to match it to the its self using RegExp methods.*/
+    const theReturnedValue = JSON.stringify(seperateMultiples(noDupes));
+    /* Here I am using "toMatch"(a RegExp testing method)
+           to confirm the array is still the same as before*/
+    expect(theReturnedValue).toMatch(JSON.stringify(noDupesStill));
+  });
 });
