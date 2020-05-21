eefca3748114a3525f4616b725e17450618a40b4
Alex Miller <alexmillerprojects@gmail.com>
2020-05-21 08:29:29 -0700

    writing tests for dictionaryParser and separateMultiples files

--- models/tests/dictionaryParcer.test.js
+++ models/tests/dictionaryParcer.test.js
@@ -1,24 +1,34 @@
 const dictionaryParcer = require("../dictionaryParcer");
 const mockData = require("./testDummyData");
 
+afterEach(() => {
+  expected = null;
+});
 
-it('should remove "market" from the commodity markets prop in the first object', () => {
-    expect(mockData[0].commoditymarket).toBe("Nairobi Market")
+describe("Data normalization is adjust the data properly", () => {
+  it('should remove "market" from the commodity markets prop in the first object', () => {
+    expect(mockData[0].commoditymarket).toBe("Nairobi Market");
+    // console.log(mockData)
 
-    const expected = dictionaryParcer(mockData)
+    const expected = dictionaryParcer(mockData);
+
     for (var i = 0; i < expected.length; i++) {
-        expect(expected[i].commoditymarket).not.toMatch(/market/i)
+      expect(expected[i].commoditymarket).not.toMatch(/market/i);
     }
-
-})
+  });
+});
 
 // console.log(mockData)
 
+//RESEARCH HOW TO CLEAR/TEAR DOWN MOCKS BETWEEN TEST RUNS
+
 it('mockData[2] object should change from "farm input" to farm inputs', () => {
-    // expect(mockData[2].procedurecommoditycat).toBe(/animal product/)
-    // const expected = dictionaryParcer(mockData)
-    // expect(expected[2].procedurecommoditycat).toBe(/Animal Products/)
+  console.log(mockData);
+  // expect(mockData[2].procedurecommoditycat).toBe(/animal product/)
+  // const expected = dictionaryParcer(mockData)
+  // expect(expected[2].procedurecommoditycat).toBe(/Animal Products/)
+});
 
-})
-
-it.todo('mockData[2] object should change ANIMAL PRODUCT and Pawpaws to be reformatted')
+it.todo(
+  "mockData[2] object should change ANIMAL PRODUCT and Pawpaws to be reformatted"
+);
--- models/tests/seperateMultiples.test.js
+++ models/tests/seperateMultiples.test.js
@@ -1,12 +1,13 @@
 const seperateMultiples = require("../seperateMultiples");
 const mockData = require("./testDummyData");
 
-// test(`creates new objects`, () => {
-//     let count = 0
-//     for (var i = 0; i < mockData.length; i++) {
-//         if ()
-//     }
-//     expect(mockData.length).toBeLessThan(seperateMultiples(mockData).length)
-// })
-
-// todo(`returns an array`)
+test(`creates new objects`, () => {
+    let count = 0
+    for (var i = 0; i < mockData.length; i++) {
+        Object.entries(mockData[i]).forEach(entry => {
+            if (typeof entry[1] === "string" && entry[1].includes(",")) {
+                ++count
+            }
+        })
+    }
+    expect((mockData.length)+count).toEqual(seperateMultiples(mockData).length)
